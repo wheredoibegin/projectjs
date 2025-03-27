94ab6bed1be2d7d7fff43e471a9323457377bc8a
wheredoibegin <95106613+wheredoibegin@users.noreply.github.com>
2025-03-27 22:40:43 +0100

    Update scripttest2.js

--- js/scripttest2.js
+++ js/scripttest2.js
@@ -53,3 +53,46 @@ sda23
 sdfdf
 sdfdf
 fgfdg
+
+
+
+//Условие true o false
+if (4 === 9) {
+    console.log("Ok!");
+} else {
+    console.log("Error");
+}
+
+//Вложеность условий
+
+const num = 50;
+
+if (num < 49) {
+    console.log("Error");
+} else if (num > 100) {
+    console.log ("Много");
+} else {
+    console.log("Ok");
+}
+
+//Тернарный оператор
+//Проверяем, если первое условие верно, то это, а если нет, то второе
+//Экономит места и объем кода
+(num === 50) ? console.log("Ok") : console.log ("Error");
+
+//конструкция свитч всегда идет на строгое сравнение
+const num = 51;
+switch (num) {
+    case 49:
+        console.log("No");
+        break;
+    case 100:
+        console.log("No");
+        break;
+    case 50:
+        console.log("Yes");
+        break; 
+    default:
+        console.log("Не в этот раз");
+        break;
+}
