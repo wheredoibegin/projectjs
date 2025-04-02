8dc181d5771375710644a17b32fdba7ac8641b8b
Denis <ithelpden@gmail.com>
2025-04-02 17:47:19 +0200

    practic2

--- js/scriptpractice.js
+++ js/scriptpractice.js
@@ -25,3 +25,49 @@ console.log(personalMovieDB);
 // const answer2 = (question2);
 // personalMovieDB.movies[question1] = question2;
 // personalMovieDB.movies[answer1] = answer2;
+
+
+
+
+// Логические операторы
+
+const hamburger = true;
+const fries = true;
+
+if(hamburger && fries) {
+    console.log("Я сыт!");
+}
+
+console.log((hamburger && fries));
+
+
+// Всегда будут фолс ноль, пустая строка, null, undefined, not a number
+const hamburger = 5;
+const fries = 0;
+
+if(hamburger && fries) {
+    console.log("Я сыт!");
+}
+
+console.log((hamburger && fries));
+
+const hamburger = 2;
+const fries = 1;
+
+if(hamburger === 3 && fries) {
+    console.log("Все сыты");
+} else {
+    console.log("Мы уходим")
+}
+
+//Это вернет false, потому что кола будет выполняться первой по приоритету оператора равно
+const hamburger = 2;
+const fries = 1;
+const cola = 0;
+
+if(hamburger === 3 && cola === 1 && fries) {
+    console.log("Все сыты");
+} else {
+    console.log("Мы уходим")
+}
+
