861f56a8f38e2a4b183ca13cf3cd2107a56347fb
Denis <ithelpden@gmail.com>
2025-04-04 16:34:30 +0200

    practic22211

--- js/scriptpractice.js
+++ js/scriptpractice.js
@@ -114,3 +114,64 @@ for (let i = 1; i < 10; i++) {
 }
 
 //итерация - повторение цикла
+
+
+
+console.log( NaN || 2 || undefined );
+
+
+console.log( NaN && 2 && undefined );
+
+
+console.log( 1 && 2 && 3 );
+
+
+console.log( !1 && 2 || !3 );
+
+
+console.log( 25 || null && !3 );
+
+
+console.log( NaN || null || !3 || undefined || 5);
+
+
+console.log( NaN || null && !3 && undefined || 5);
+
+
+console.log( 5 === 5 && 3 > 1 || 5);
+
+
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 0;
+
+
+if (hamburger === 3 && cola || fries === 3 && nuggets) {
+   console.log('Done!')
+   } else {
+    console.log("None!");
+   }
+
+
+   let hamburger;
+   const fries = NaN;
+   const cola = 0;
+   const nuggets = 2;
+   
+   
+   if (hamburger || cola || fries === 3 || nuggets) {
+      console.log('Done!')
+   }
+   
+
+   let hamburger;
+   const fries = NaN;
+   const cola = 0;
+   const nuggets = 2;
+   
+   
+   if (hamburger && cola || fries === 3 && nuggets) {
+      console.log('Done!')
+   }
+   
