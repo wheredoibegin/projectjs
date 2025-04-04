5bcf3493d4ddc75981797d145cd6cb0d103b82dd
Denis <ithelpden@gmail.com>
2025-04-04 16:16:01 +0200

    practic2221

--- js/scriptpractice.js
+++ js/scriptpractice.js
@@ -71,3 +71,46 @@ if(hamburger === 3 && cola === 1 && fries) {
     console.log("Мы уходим")
 }
 
+//Циклы
+let num = 50;
+//пока наши условия не выполнены - выполнять такие-то действия
+while (num <= 55) {
+    console.log(num);
+    num++;
+}
+
+//2 вариант
+let num = 50;
+
+// сделай
+do {
+    console.log(num);
+    num++;
+}
+//после того как цикл отработал проверяем условие
+while (num < 55);
+
+
+//цикл for популярный
+for (let i = 1; i < 8; i++) {
+    console.log(i);
+}
+//будет повторяться 7 раз
+let num = 50;
+for (let i = 1; i < 8; i++) {
+    console.log(num);
+    num++;
+}
+
+//завершить цикл
+
+let num = 50;
+for (let i = 1; i < 10; i++) {
+    if (i === 6) {
+        //break;
+        continue;
+    }
+    console.log(i);
+}
+
+//итерация - повторение цикла
