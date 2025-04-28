1cbd55c34674ec47def436499d51f5d03d80a79c
Denis <ithelpden@gmail.com>
2025-04-28 22:52:39 +0200

    second commitnewst2

--- js/www.js
+++ js/www.js
@@ -180,3 +180,19 @@ function thirdTask() {
 // *********
 // ***********
 
+
+
+
+
+let result = "";
+const levels = 6;
+for (let i = 0; i <= levels; i++) {
+    for (let j = 0; j < levels - i; j++) {
+        result += " ";
+    }
+for (let j = 0; j < 2 * i + 1; j++) {
+        result += "*";
+}
+    result += "\n";
+}
+console.log (result);
